df718be3dff6469b5c49173f9bda6706819400b1
rafaelmardones <rafaelm@rdon.es>
2022-08-21 18:35:38 +0200

    Enable static files

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -17,6 +17,7 @@ func routes(app *config.AppConfig) http.Handler {
 	r.Use(SessionLoad)
 	r.Get("/", handlers.Repo.Home)
 	r.Get("/about", handlers.Repo.About)
-
+	fs := http.FileServer(http.Dir("./static"))
+	r.Handle("/static/*", http.StripPrefix("/static", fs))
 	return r
 }
